c0e80fef87f0ceba22ab0bd14941d04b98f4d2c6
JoshX <joshx.dev@gmail.com>
2023-07-03 00:06:09 +0100

    feat: fetch & filter recipes by query

---
+++ src/app/components/recipe-list.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { FormEvent, Suspense, useRef, useState } from "react";
+import { Recipe, Recipes } from "../types";
+import { buildQueryParams, getHeaders } from "../utils/helpers";
+import Card from "./card";
+import Search from "./search";
+
+type Props = {
+  recipes: Recipe[];
+};
+const RecipeList = async ({ recipes: fetchedRecipes }: Props) => {
+  const [recipes, setRecipes] = useState<Recipe[]>(fetchedRecipes);
+  const queryRef = useRef<HTMLInputElement>(null);
+
+  const headers = getHeaders();
+
+  const onSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const params: { [key: string]: string } = {};
+
+    if (queryRef.current !== null) {
+      params.query = queryRef.current.value;
+
+      const queryParams = buildQueryParams(params);
+
+      const res = await fetch(`/api/recipe${queryParams}`, { headers });
+      const response: { data: Recipes } = await res.json();
+
+      setRecipes(response.data.results || []);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col md:flex-row md:items-center justify-between">
+        <h2 className="text-xl font-bold">All Recipes</h2>
+        <Search onSubmit={onSearch} ref={queryRef} />
+      </div>
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <Suspense fallback={<div>Loading...</div>}>
+          {recipes.length > 0 ? (
+            recipes.map((recipe, i) => (
+              <div key={i}>
+                <Card>
+                  <Link href="/">
+                    <Image
+                      src={recipe?.image}
+                      alt={recipe?.title}
+                      width={100}
+                      height={100}
+                      className="w-full md:w-full h-w-fit"
+                    />
+                  </Link>
+                  <Link href="/">
+                    <p className="text-sm mt-4">{recipe?.title}</p>
+                  </Link>
+                </Card>
+              </div>
+            ))
+          ) : (
+            <span>No recipes available</span>
+          )}
+        </Suspense>
+      </div>
+    </>
+  );
+};
+
+export default RecipeList;
---
+++ src/app/components/search.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from "react";
+
+type Props = {
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const Search = forwardRef<HTMLInputElement, Props>(({ onSubmit }, ref) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="flex">
+        <input
+          type="text"
+          className="bg-transparent border border-teal-900 focus-visible:border-black h-11 outline-none px-4"
+          ref={ref}
+        />
+        <button type="submit" className="bg-green-700 text-white p-[10px]">
+          search
+        </button>
+      </div>
+    </form>
+  );
+});
+
+export default Search;
+
+Search.displayName = "Search";
