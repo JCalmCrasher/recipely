a80325fee318bb50c2f4297513fc881622dc7c4c
JoshX <joshx.dev@gmail.com>
2023-07-04 15:09:19 +0100

    style the summary anchor tags

--- src/app/recipes/[recipeId]/page.tsx
+++ src/app/recipes/[recipeId]/page.tsx
@@ -15,7 +15,7 @@ const RecipeInfo = async ({ params }: ParamsProps) => {
       <h2 className="text-xl font-bold">{recipe.title}</h2>
       <Card shouldHover={false} classNames="w-full min-h-[200px]">
         <h3 className="text-md font-semibold">About</h3>
-        <div className="mt-3" dangerouslySetInnerHTML={{ __html: recipe.summary }} />
+        <div className="mt-3 summary" dangerouslySetInnerHTML={{ __html: recipe.summary }} />
       </Card>
     </Shell>
   );
