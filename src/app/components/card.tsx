fcfc496cdfbc179f1d2f46e5fca71c32e7b2ead6
JoshX <joshx.dev@gmail.com>
2023-07-04 14:38:57 +0100

    feat: `props` to add hover style

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,8 +1,22 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
-const Card = ({ children }: { children: ReactNode }) => {
+type CardProps = {
+  children: ReactNode;
+  shouldHover?: boolean;
+  classNames?: string;
+};
+const Card = ({ classNames, children, shouldHover = true }: CardProps) => {
   return (
-    <div className="border border-black transition-all duration-700 ease-in-out hover:shadow-[5px_5px_0px_black] px-3 py-4 mt-7">
+    <div
+      className={clsx(
+        `border border-[#3A2F2F] transition-all duration-300 ease-in-out px-3 py-4 mt-7`,
+        shouldHover
+          ? "hover:shadow-[5px_5px_0px_#3A2F2F]"
+          : "shadow-[5px_5px_0px_#3A2F2F]",
+        classNames
+      )}
+    >
       {children}
     </div>
   );
