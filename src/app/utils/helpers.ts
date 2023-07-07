84e1edc423c001d1f8ae7e08a55c06084ca67f86
JoshX <joshx.dev@gmail.com>
2023-07-07 22:02:48 +0100

    feat(util): return improper fraction/whole number from a given number

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -16,3 +16,24 @@ export const getHeaders = (): Headers => {
   headers.append("Content-Type", "application/json");
   return headers;
 };
+
+const greatestCommonDivisor = (a: number, b: number): number => {
+  if (b === 0) {
+    return a;
+  }
+  return greatestCommonDivisor(b, a % b);
+};
+
+export const decimalToFraction = (decimal: number) => {
+  if (Number.isInteger(decimal)) {
+    return decimal.toString(); // Return the whole number as a string
+  }
+
+  const numerator = decimal * 100;
+  const denominator = 100;
+  const gcd = greatestCommonDivisor(numerator, denominator);
+  const simplifiedNumerator = numerator / gcd;
+  const simplifiedDenominator = denominator / gcd;
+
+  return `${simplifiedNumerator}/${simplifiedDenominator}`;
+};
