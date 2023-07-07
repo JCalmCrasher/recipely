fffff72c5309a41937cc9965935d1aec89933384
JoshX <joshx.dev@gmail.com>
2023-07-07 23:28:58 +0100

    some mobile responsivenss on the header

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,9 +7,9 @@ import clsx from "clsx";
 const Navbar = () => {
   return (
     <header className={clsx(p, "font-panchang")}>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col md:flex-row justify-between items-center">
         <Image src="/logo.svg" alt="logo" width={100} height={100} />
-        <nav className="hidden sm:flex gap-7">
+        <nav className="flex gap-4 md:gap-7">
           <NavLink href="/">home</NavLink>
           <NavLink href="/recipes">recipes</NavLink>
         </nav>
